4cf70be8666c1539eb1d494eda7f25aa08daf31d
dommmy200 <constantx003@gmail.com>
2024-09-30 12:14:01 +0100

    Added files for FormData examples

---
+++ form-example/scripts/form.js
@@ -0,0 +1,12 @@
+formElem.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let response = await fetch('/article/formdata/post/user', {
+      method: 'POST',
+      body: new FormData(formElem)
+    });
+
+    let result = await response.json();
+
+    alert(result.message);
+  };
